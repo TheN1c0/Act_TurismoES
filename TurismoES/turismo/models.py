6502fb18d520540c57d5e9e5cd3473e09358a7e1
Nicolas Bravo <nicojjj4@gmail.com>
2024-06-06 13:12:23 -0400

    Se modificó models para crear un modelo para imagenes

--- TurismoES/turismo/models.py
+++ TurismoES/turismo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+""" Clase creada por la importación de una clase
+para tomar como modelo, nombre imagen y dirección """
+class ImageModel(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+
+    """ Instancia del objeto interpretada como cadena """
+    def __str__(self):
+        return self.title
