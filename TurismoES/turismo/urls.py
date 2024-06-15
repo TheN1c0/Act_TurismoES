00808d12773731e6cce6fb8cd529ef6d6d0aa2e1
Nicolas Bravo <nicojjj4@gmail.com>
2024-06-15 03:21:10 -0400

    Se agregó un enlace en la página index que lleva a mostrar las imagenes(pagina listar) almancenadas en la base de datos

--- TurismoES/turismo/urls.py
+++ TurismoES/turismo/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('index', views.index, name='index'),path('listadoSQL', views.listadoSQL, name='listadoSQL'),]
+urlpatterns = [path('index', views.index, name='index'),path('listadoSQL', views.listadoSQL, name='listadoSQL'), path('listar', views.listar, name='listar'),]
--- TurismoES/turismo/views.py
+++ TurismoES/turismo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Alumno, Genero
+from .models import Alumno, Genero, Servicios
 # Create your views here.
 
 def index(request):
@@ -11,3 +11,8 @@ def listadoSQL(request):
     Alumnos= Alumno.objects.raw('SELECT * FROM turismo_alumno')
     context={"Alumnos":Alumnos}
     return render(request, 'turismo/listadoSQL.html', context)
+
+def listar(request):
+    Servicio= Servicios.objects.all()
+    context={"Servicios":Servicio}
+    return render(request, 'turismo/listar.html', context)
