5f1b55bc12472ea5409e3f6fad4a68c04f175698
Nicolas Bravo <nicojjj4@gmail.com>
2024-06-17 22:17:16 -0400

    Se mejoró el css

--- TurismoES/turismo/urls.py
+++ TurismoES/turismo/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
 
-urlpatterns = [path('index', views.index, name='index'),path('listadoSQL', views.listadoSQL, name='listadoSQL'), path('listar', views.listar, name='listar'), path('registrarServicio', views.registrarServicio,  name='registrarServicio'),]
+urlpatterns = [path('index', views.index, name='index'),path('listadoSQL', views.listadoSQL, name='listadoSQL'), path('listar', views.listar, name='listar'), path('registrarServicio', views.registrarServicio,  name='registrarServicio'),path('',views.index, name='index'),]
